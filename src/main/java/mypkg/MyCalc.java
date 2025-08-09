666fc5107cf40d5e34317fe218558007a75cea0e
Dr. Hitesh Kumar Sharma <hkshitesh@gmail.com>
2025-08-09 20:35:50 +0530

    Update MyCalc.java

--- src/main/java/mypkg/MyCalc.java
+++ src/main/java/mypkg/MyCalc.java
@@ -24,7 +24,7 @@ public class MyCalc {
 	}
 	public void branch()
 	{
-		System.out.println("Hello from Master Branch, Hello GitHub Actions");
+		System.out.println("Hello from Master Branch, Hello GitHub Actions, Hello from Slave2");
 	}
 
 	public static void main(String[] args) {
